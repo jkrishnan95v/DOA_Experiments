e964d8314541f64e8f4edb691a0da36bd32b6a8d
jkrishnan95v <jkrishnan95v@gmail.com>
2021-04-06 07:49:09 -0600

    Add files via upload

---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/john/libad9361-iio/test
+# Build directory: /home/john/libad9361-iio/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(FilterDesignerTest "/home/john/libad9361-iio/test/FilterDesignerTest")
+set_tests_properties(FilterDesignerTest PROPERTIES  WORKING_DIRECTORY "/home/john/libad9361-iio/test")
+add_test(GenerateRatesTest "/home/john/libad9361-iio/test/GenerateRatesTest")
+set_tests_properties(GenerateRatesTest PROPERTIES  WORKING_DIRECTORY "/home/john/libad9361-iio/test")
+add_test(FilterDesignerHardwareTest "/home/john/libad9361-iio/test/FilterDesignerHardwareTest")
+set_tests_properties(FilterDesignerHardwareTest PROPERTIES  WORKING_DIRECTORY "/home/john/libad9361-iio/test")
+add_test(FMComms5SyncTest "/home/john/libad9361-iio/test/FMComms5SyncTest")
+set_tests_properties(FMComms5SyncTest PROPERTIES  WORKING_DIRECTORY "/home/john/libad9361-iio/test")
+add_test(AutoRateTest "/home/john/libad9361-iio/test/AutoRateTest")
+set_tests_properties(AutoRateTest PROPERTIES  WORKING_DIRECTORY "/home/john/libad9361-iio/test")
