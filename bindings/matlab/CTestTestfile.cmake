46e8aedd24868aab1ea3c5dbf272894ee8cb254f
jkrishnan95v <jkrishnan95v@gmail.com>
2021-04-06 07:44:39 -0600

    Add files via upload

---
+++ bindings/matlab/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/john/libad9361-iio/bindings/matlab
+# Build directory: /home/john/libad9361-iio/bindings/matlab
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
